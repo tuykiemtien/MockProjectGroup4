0c5cc6b1f5c907a26baaecbf7256eca890b61afc
Nguyễn Ngọc Sơn <tuykiemtien@gmail.com>
2018-03-01 16:34:17 +0700

    Revert "Revert "version 1""
    This reverts commit 9bab7fbebf319af29b1b5bb78ab582d48f5a7ae9.

---
+++ MockProjectGroup4/DTO/CategoryDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class CategoryDTO
+    {
+        public int CategoryID { get; set; }
+        public string CategoryName { get; set; }
+        public string Description { get; set; }
+        public byte[] Picture { get; set; }
+        public virtual ICollection<ProductDTO> Products { get; set; }
+
+    }
+}
---
+++ MockProjectGroup4/DTO/ShipperDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class ShipperDTO
+    {
+        public int ShipperID { get; set; }
+        public string CompanyName { get; set; }
+        public string Phone { get; set; }
+
+        
+        public virtual ICollection<OrderDTO> Orders { get; set; }
+    }
+}
