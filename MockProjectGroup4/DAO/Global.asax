9bab7fbebf319af29b1b5bb78ab582d48f5a7ae9
Nguyễn Ngọc Sơn <tuykiemtien@gmail.com>
2018-03-01 16:33:05 +0700

    Revert "version 1"
    This reverts commit 751efba29d8deaad23d6e19070d6ab9fcf9b9406.

--- MockProjectGroup4/DAO/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DAO.WebApiApplication" Language="C#" %>
--- MockProjectGroup4/Query.sql
+++
@@ -1,9 +0,0 @@
-USE Northwind
-CREATE TABLE Account
-(
-	[Username] VARCHAR(20) PRIMARY KEY,
-	[Password] VARCHAR(20) NOT NULL,
-	CustomerId NCHAR(5) REFERENCES Customers(CustomerId),
-	EmployeeId INT REFERENCES Employees(EmployeeId)
-
-)
